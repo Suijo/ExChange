58fda027ac8805cd8aa0eb6d1bb09191051c2a0b
Suijo <bogergo9@gmail.com>
2021-05-26 20:23:47 +0200

    Another way added to writefile

--- src/main/java/Dao/CurrencyDao.java
+++ src/main/java/Dao/CurrencyDao.java
@@ -40,7 +40,13 @@ public class CurrencyDao{
      * @return
      * @throws IOException
      */
-
+    /*private String ReadFile() throws IOException{
+        try (BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("/players.json").getFile()))) {
+            return reader.lines().collect(Collectors.joining());
+        } catch (IOException e) {
+            throw new IOException("Can not read player data");
+        }
+    }*/
     private String ReadSaveFile() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("/saved.json").getFile()))) {
             return reader.lines().collect(Collectors.joining());
@@ -52,7 +58,35 @@ public class CurrencyDao{
         }
 
     }
+    public static void SaveCurr(String fromval, String toval, double moneyval,String sellorbuyval) throws IOException, ParseException {
 
+        JSONParser jsonParser = new JSONParser();
+        Object obj =jsonParser.parse(new FileReader("src/main/resources/saved.json"));
+        JSONArray jsonArray = (JSONArray)obj;
+
+        JSONObject CurrencyData = new JSONObject();
+        CurrencyData.put("from",fromval);
+        CurrencyData.put("to",toval);
+        CurrencyData.put("money",moneyval);
+        CurrencyData.put("sellorbuy",sellorbuyval);
+
+
+
+
+        jsonArray.add(CurrencyData);
+
+        try (FileWriter file = new FileWriter("src/main/resources/saved.json")) {
+
+            file.append(jsonArray.toJSONString());
+            file.flush();
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+            Logger.error(new IOException("Ooops"),"IO hiba tortent");
+        }
+        Logger.info("added");
+    };
 
 
 
@@ -74,12 +108,12 @@ public class CurrencyDao{
         Logger.info("A WriteFile called.");
     }
 
-    public void SaveCurr(String fromval, String toval, double moneyval,String sellorbuyval) throws IOException {
+    /*public void SaveCurr(String fromval, String toval, double moneyval,String sellorbuyval) throws IOException {
         Saved[] saved = GetSaveModel();
         WriteFile( ggson.toJson( ArrayUtils.add(saved,new Saved(fromval,toval,moneyval,sellorbuyval))));
         Logger.info("Currency is saved.");
         System.out.println(ggson.toJson( ArrayUtils.add(saved,new Saved(fromval,toval,moneyval,sellorbuyval))));
-    }
+    }*/
 
 
     /**
